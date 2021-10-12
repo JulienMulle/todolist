c809c1ff1565633cd5caba1c59c4031c45851e39
Julien <julienmulle@gmail.com>
2021-10-12 16:30:18 +0200

    création de la date

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
+const days = ["Dimanche", "Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"]
+const month = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"]
+
 export default function Header() {
+    const date = new Date()
     return (
         <>
-            <Text> {Date.now().toString()}</Text>
+            <Text> {`${days[date.getDay()]} ${date.getDate()} ${month[date.getMonth()]}`}</Text>
         </>
     )
 }
