a8ea777ef132663216b2449eb5b82c25e31b258d
Julien <julienmulle@gmail.com>
2021-10-12 16:45:10 +0200

    test du useState tasks

--- src/screens/tasks/index.js
+++ src/screens/tasks/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text } from 'react-native';
 import Header from '../../components/Header';
 
 export default function TaskScreen() {
+    // Liste de taches
+    //State pour garder en mémoire les taches
+    const [tasks, setTasks] = useState([
+        {title: "Hello l'ami du bon gout !", isCompleted: false}
+    ])
+    //asyncStorage
+
     return (
         <>
             <Header />
+            {tasks.map(t => <Text>{t.title}</Text>)}
         </>
     )
 }
