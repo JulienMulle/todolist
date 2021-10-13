90a0124844ca4ce30723269a37a6f3859781ee96
Julien <julienmulle@gmail.com>
2021-10-13 10:31:20 +0200

    creation du component taskTile

--- src/screens/tasks/index.js
+++ src/screens/tasks/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, FlatList } from 'react-native';
 import Header from '../../components/Header';
+import TaskTile from './TaskTile';
 
 export default function TaskScreen() {
     // Liste de taches
@@ -11,13 +12,16 @@ export default function TaskScreen() {
 
     //item sera un élément du tableau : {title: "Hello l'ami du bon gout !", isCompleted: false}
     const renderItem = ({item}) => {
-        return <Text>{item.title}</Text>
+        return <TaskTile task={item}/>
     }
     //asyncStorage
 
     return ( 
             <FlatList 
+            //scroll infini avec chargement des données
+            onEndReached={()=> loadData}
             ListHeaderComponent={<Header />}
+            contentContainerStyle={{flexGrow:1}}
             data={tasks}
             //si je n'ai pas d'id, j'utilise la fonction de keyExtractore qui va recevoir item qui a l'index du tableau, avec la methode toString, on aura un index unique parce que chaques index dans un tableau est unique
 
