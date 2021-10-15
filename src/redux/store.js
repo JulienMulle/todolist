6c1fd0eabb0d44da533bc5c0ec9d7a74db0f5fbd
Julien <julienmulle@gmail.com>
2021-10-15 10:46:17 +0200

    creation du reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,18 @@ import { createStore } from "redux";
 //Actions: fonctions qui renvoient un objet aux reducers - {type: MON_ACTION, payload : {DATA}}
 
 //Reducers: fonctions retournent un nouveau
+const initialState = [{id:1, title:"Init task", completed: false}]
+
+const tasksList = (state= initialState, action) => {
+    switch(action.type) {
+       // case MON_ACTION:
+            //modifs
+         //   return newState
+        
+        default:
+            return state
+    }
+}
 
 //Store: attaché à Réact -accessible depuis tout les composants
 export const store = createStore()
