a2c4b7a6228e987439cf315193ffc49f01af473e
Julien <julienmulle@gmail.com>
2021-10-15 11:02:40 +0200

    création de l' actions ADD_TASK

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,34 @@ import { createStore, combineReducers } from "redux";
 
 //Actions: fonctions qui renvoient un objet aux reducers - {type: MON_ACTION, payload : {DATA}}
 
+//action ajout d'une tache
+
+// stockant mon string dans une const, je reduis le nombre d'erreur
+const ADD_TASK = "ADD_TASK"
+function addTask(title) {
+    return{
+        type: ADD_TASK,
+        payload:{
+            //on peut aussi le voir comme ca = title: title. grace à es6 je peux l'ecrire title           
+            title
+        }
+    }
+}
+//action changement status d'une tache
+
+//action suppression de la tache
+
 //Reducers: fonctions retournent un nouveau
 const initialState = [{id:1, title:"Init task", completed: false}]
 
 const tasksList = (state= initialState, action) => {
     switch(action.type) {
-       // case MON_ACTION:
-            //modifs
-         //   return newState
+       case ADD_TASK:
+           return [...state,{
+               id: new Date().getTime(),
+               title: action.payload.title,
+               completed: isCompleted
+           }]
         
         default:
             return state
